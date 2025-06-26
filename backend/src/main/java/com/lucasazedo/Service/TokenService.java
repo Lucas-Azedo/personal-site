be388f7580c18819181dd29af011e165be8d1e22
Lucas Alberto Azevedo da Silva <lucas.0707.oliveira@gmail.com>
2025-06-26 14:29:11 -0300

    Validate token

--- backend/src/main/java/com/lucasazedo/Service/TokenService.java
+++ backend/src/main/java/com/lucasazedo/Service/TokenService.java
@@ -2,6 +2,8 @@ package com.lucasazedo.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.lucasazedo.Model.User;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +24,17 @@ public class TokenService {
                 .withClaim("email", user.getEmail())
                 .withExpiresAt(Instant.now().plus(1, ChronoUnit.HOURS))
                 .sign(ALGORITHM);
+    }
+
+    public String validateToken(String token){
+        try{
+            DecodedJWT decodedJWT = JWT.require(ALGORITHM)
+                    .build()
+                    .verify(token);
+            return decodedJWT.getSubject();
+        }
+        catch(JWTVerificationException e){
+            throw new JWTVerificationException("Token inválido");
+        }
     }
 }
