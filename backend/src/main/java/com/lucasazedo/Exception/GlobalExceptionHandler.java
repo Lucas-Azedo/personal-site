0d85431581c865c8d10cfd222e1e5b28cef0c08f
Lucas-Azedo <2007.lucas.azevedo@gmail.com>
2025-06-26 22:27:54 -0300

    feat: GlobalExceptionHandler

---
+++ backend/src/main/java/com/lucasazedo/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.lucasazedo.Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException e){
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error ->{
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
